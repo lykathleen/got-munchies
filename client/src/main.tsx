815c92e26bfa9bece151c591035934e9f1449cfc
lykathleen <lykathleen@users.noreply.github.com>
2024-08-20 21:44:43 +0100

    Implemented Auth0

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './global.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
+import AuthProviderWithNavigate from './auth/AuthProviderWithNavigate';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <AppRoutes />
+      <AuthProviderWithNavigate>
+        <AppRoutes />
+      </AuthProviderWithNavigate>
     </Router>
   </StrictMode>
 );
