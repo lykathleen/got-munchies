681b8ff7ea37de78a4e956bc277d78b64e844c51
lykathleen <lykathleen@users.noreply.github.com>
2024-08-21 22:03:24 +0100

    Created hook to handle fetch request

--- client/src/auth/AuthProviderWithNavigate.tsx
+++ client/src/auth/AuthProviderWithNavigate.tsx
@@ -1,11 +1,14 @@
 import React, { ReactNode } from 'react';
 import { AppState, Auth0Provider, User } from '@auth0/auth0-react';
+import { useCreateMyUser } from '@/api/MyUserApi';
 
 interface Props {
   children: ReactNode;
 }
 
 const AuthProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -15,6 +18,10 @@ const AuthProviderWithNavigate = ({ children }: Props) => {
   }
 
   const onRedirectCallback = (appState?: AppState, user?: User) => {
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
+
     console.log('user', user);
   };
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,24 @@ import './global.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import AuthProviderWithNavigate from './auth/AuthProviderWithNavigate';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-      <AuthProviderWithNavigate>
-        <AppRoutes />
-      </AuthProviderWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <AuthProviderWithNavigate>
+          <AppRoutes />
+        </AuthProviderWithNavigate>
+      </QueryClientProvider>
     </Router>
   </StrictMode>
 );
