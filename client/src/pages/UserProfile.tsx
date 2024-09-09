82a377f3108310362fca435737e5805e97aa0167
lykathleen <lykathleen@users.noreply.github.com>
2024-09-09 22:50:16 +0100

    Created an update user profile function and connect to api

--- client/src/pages/UserProfile.tsx
+++ client/src/pages/UserProfile.tsx
@@ -1,8 +1,11 @@
+import { useUpdateMyUser } from '@/api/MyUserApi';
 import UserProfileForm from '@/forms/user-profile-form/UserProfileForm';
 import React from 'react';
 
 const UserProfile = () => {
-  return <UserProfileForm />;
+  const { updateUser, isLoading } = useUpdateMyUser();
+
+  return <UserProfileForm onSave={updateUser} isLoading={isLoading} />;
 };
 
 export default UserProfile;
