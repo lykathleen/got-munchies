77274eeb1fcb463f2c8da006a304c48da73b1543
lykathleen <lykathleen@users.noreply.github.com>
2024-09-23 22:31:52 +0100

    Authentication and validation for restaurant

--- server/src/middleware/validation.ts
+++ server/src/middleware/validation.ts
@@ -22,3 +22,22 @@ export const validateMyUserRequest = [
   body('city').isString().notEmpty().withMessage('City must be a string'),
   handleValidationErrors,
 ];
+
+export const validateMyRestaurantRequest = [
+  body('restaurantName').notEmpty().withMessage('Restaurant name is required'),
+  body('city').notEmpty().withMessage('City is required'),
+  body('country').notEmpty().withMessage('Country is required'),
+  body('deliveryPrice')
+    .isFloat({ min: 0 })
+    .withMessage('Delivery price must bea postiive number'),
+  body('estimatedDeliveryTime')
+    .isInt({ min: 0 })
+    .withMessage('Estimated delivery time must be a positive number'),
+  body('cuisines').isArray().withMessage('Cuisines array cannot be empty'),
+  body('menuItems').isArray().withMessage('Menut items must be an array'),
+  body('menuItems.*.name').isEmpty().withMessage('Menu item name is required'),
+  body('menuItems.*.price')
+    .isFloat({ min: 0 })
+    .withMessage('Menu item price is required'),
+  handleValidationErrors,
+];
--- server/src/routes/MyRestaurantRoute.ts
+++ server/src/routes/MyRestaurantRoute.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import multer from 'multer';
 import MyRestaurantController from '../controllers/MyRestaurantController';
+import { jwtCheck, jwtParse } from '../middleware/auth';
+import { validateMyRestaurantRequest } from '../middleware/validation';
 
 const router = express.Router();
 
@@ -16,6 +18,9 @@ const upload = multer({
 router.post(
   '/',
   upload.single('imageFile'),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
   MyRestaurantController.createMyRestaurant
 );
 
