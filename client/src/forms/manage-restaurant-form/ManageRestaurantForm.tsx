845609725b81b16a95eed2c80fa4b417a41a5757
lykathleen <lykathleen@users.noreply.github.com>
2024-10-08 22:55:16 +0100

    Created image section and loading state in button

--- client/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ client/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -6,6 +6,9 @@ import DetailsSection from './DetailsSection';
 import { Separator } from '@radix-ui/react-separator';
 import CuisinesSection from './CuisinesSection';
 import MenuSection from './MenuSection';
+import ImageSection from './ImageSection';
+import LoadingButton from '@/components/LoadingButton';
+import { Button } from '@/components/ui/button';
 
 const formSchema = z.object({
   restaurantName: z.string({
@@ -65,6 +68,9 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
         <CuisinesSection />
         <Separator />
         <MenuSection />
+        <Separator />
+        <ImageSection />
+        {isLoading ? <LoadingButton /> : <Button type="submit">Submit</Button>}
       </form>
     </Form>
   );
