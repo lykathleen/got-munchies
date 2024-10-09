fc9877aa7ca0efbe7ada6cd384d54d4d700611ec
lykathleen <lykathleen@users.noreply.github.com>
2024-10-09 22:52:07 +0100

    Created custom hook to call API post request

--- client/src/pages/ManageRestaurant.tsx
+++ client/src/pages/ManageRestaurant.tsx
@@ -1,7 +1,12 @@
+import { useCreateMyRestaurant } from '@/api/MyRestaurantApi';
 import ManageRestaurantForm from '@/forms/manage-restaurant-form/ManageRestaurantForm';
 
 const ManageRestaurant = () => {
-  return <ManageRestaurantForm />;
+  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+
+  return (
+    <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+  );
 };
 
 export default ManageRestaurant;
--- client/src/types.ts
+++ client/src/types.ts
@@ -1,8 +1,27 @@
-export type User = {
+export interface User {
   _id: string;
   email: string;
   name: string;
   address: string;
   city: string;
   country: string;
-};
+}
+
+interface MenuItem {
+  _id: string;
+  name: string;
+  number: string;
+}
+export interface Restaurant {
+  _id: string;
+  user: string;
+  restaurantName: string;
+  city: string;
+  country: string;
+  deliveryPrice: number;
+  estimatedDeliveryTime: number;
+  cuisines: string[];
+  menuItems: MenuItem[];
+  imageUrl: string;
+  lastUpdated: string;
+}
